2ffbd0e340f62c758be5a3236ea036c9f73b02b8
prabhat1510 <prabhatc@gmail.com>
2023-01-15 11:49:54 +0530

    Adding Selenium project

--- 15012022_classwork_selenium/firstseleniumtestproj/src/test/java/firstseleniumtestproj/InstallDriversTest.java
+++ 15012022_classwork_selenium/firstseleniumtestproj/src/test/java/firstseleniumtestproj/InstallDriversTest.java
@@ -18,21 +18,29 @@ public class InstallDriversTest {
 	public void chromeSession() {
 		//Setting up the session to chrome browser
 		WebDriverManager.chromedriver().setup();
-
+		
+		//Initializing the Chrome Driver
 		WebDriver driver = new ChromeDriver();
+		//Passing the url to be tested or opened
 		driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+		//Using driver trying to get title of the web page opened above
 		String title = driver.getTitle();
+		//Assert actual title with the expected title
         assertEquals("Web form", title);
-        System.out.println("Assertion passed");
-      
+        //System.out.println("Assertion passed");
+        //Timeout for 1500 ms implicitily wait 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1500));
 
+        //Using driver's findElement method or function we are trying to find my-text element by name
         WebElement textBox = driver.findElement(By.name("my-text"));
         WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
+        
+        //Using webelement sendkeys method we are sending text "Selenium" and then submitting the button
+        //Sending text means we are testing the Input text box element of html page
         textBox.sendKeys("Selenium");
         submitButton.click();
-   
+        
+        //After submitting we are getting response Received! which will be referred by html element id "message"
         WebElement message = driver.findElement(By.id("message"));
         String value = message.getText();
         assertEquals("Received!!!!", value);
